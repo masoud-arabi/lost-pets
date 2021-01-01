bcbb789c3778872a062fd85342f3a90f34a329c4
masoud arabi <arabimasoud88@gmail.com>
2021-01-01 13:57:16 -0500

    all action controller is done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,16 +11,32 @@ class PetsController < ApplicationController
     def new
         @pet = Pet.new
     end
-
+    
     def create
+        @pet = Pet.new(pet_params)
+        @pet.save
+        redirect_to pet_path(@pet)
     end
-
+    
     def edit
+        @pet = Pet.find(params[:id])
     end
-
+    
     def update 
+        @pet = Pet.find(params[:id])
+        @pet.update(pet_params)
+        redirect_to pet_path(@pet)
+    end
+    
+    def destroy
+        @pet = Pet.find(params[:id])
+        @pet.destroy
+        redirect_to root_path
     end
 
-    def destroy
+    private
+
+    def pet_params
+        params.require(:pet).permit(:name, :address, :spacies, :found_on)
     end
 end
